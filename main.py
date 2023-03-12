5281792b70aba6c0a6f9feb03465a11d7b1e1ae6
askld135 <w3218822@naver.com>
2023-03-12 15:42:09 +0900

    얼굴이 인식되면 이미지등록 된 사람은 이름을 띄우나 c:; cd 'c:\Users\VisionLab\Desktop\Hw_2'; & 'C:\Users\VisionLab\anaconda3\envs\face\python.exe' 'c:\Users\VisionLab\.vscode\extensions\ms-python.python-2023.4.1\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '56256' '--' 'C:\Users\VisionLab\Desktop\Hw_2\real_time.py'

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #"https://github.com/ageitgey/face_recognition/blob/master/README_Korean.md"
+#"https://ukayzm.github.io/unknown-face-classifier/"
 
 import cv2
 import face_recognition
+from simple_facerec import SinpleFacerec
 
 img = cv2.imread("Michael_Douglas.jpg")
 rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img_encoding = face_recognition.face_encodings(rgb_img)[0]
 
-img2 = cv2.imread("images/Michael_Douglas.jpg")
+img2 = cv2.imread("images/Keum-Kang-seon.jpg")
 rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
 img_encoding2 = face_recognition.face_encodings(rgb_img2)[0]
 
