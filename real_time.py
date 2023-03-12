2f547e77d0ceef98b8f8f8af7b572dde5acc1f7f
askld135 <w3218822@naver.com>
2023-03-12 15:33:44 +0900

    얼굴이 인식되면 외곽선을 띄우고 이름을 출력하는 부분까지 구현

--- real_time.py
+++ real_time.py
@@ -14,7 +14,10 @@ while True:
     #Detect Faces
     face_locations, face_names = sfr.detect_known_faces(frame)
     for face_loc, name in zip(face_locations, face_names):
-        print(face_loc)
+        top, left, bottom, right = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+        #y1, x1, y2, x2
+        cv2.putText(frame, name,(left,top - 10),cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 2)
+        cv2.rectangle(frame, (left, top), (right, bottom),(230, 230, 250), 4)
     
     cv2.imshow("Frame", frame)
     
